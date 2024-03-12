7f662593760787fb1b24248b7b2e29f68e884361
zy <varzy@live.com>
2024-03-12 15:48:01 +0800

    Update blog pages revalidate time.

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -1,7 +1,7 @@
 import BlogHeader from '@/app/(blog)/_components/BlogHeader';
 import BlogFooter from '@/app/(blog)/_components/BlogFooter';
 
-export const revalidate = 3600; // 1hour.
+export const revalidate = 1800; // half hour.
 
 export default function BlogLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (

6fe1c28e9e80abdbf0b67880d32b4d55cc89ff88
zy <varzy@live.com>
2024-05-09 17:53:54 +0800

    Update styles.

--- src/app/_components/HomeWrapper.tsx
+++ src/app/_components/HomeWrapper.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
-import { FaRegPenToSquare } from 'react-icons/fa6';
-import { FaGithub, FaInstagram, FaRegAddressCard, FaTelegramPlane } from 'react-icons/fa';
-import { SiDoubanread } from 'react-icons/si';
-import { RiNeteaseCloudMusicLine } from 'react-icons/ri';
+import {ReactNode} from 'react';
+import {FaRegPenToSquare} from 'react-icons/fa6';
+import {FaGithub, FaInstagram, FaRegAddressCard, FaTelegramPlane} from 'react-icons/fa';
+import {SiDoubanread} from 'react-icons/si';
+import {RiNeteaseCloudMusicLine} from 'react-icons/ri';
 import LocaleToggle from '@/app/_components/LocaleToggle';
 import Avatar from '@/app/_components/Avatar';
-import FancyLink, { FancyLinkProps } from '@/app/_components/FancyLink';
+import FancyLink, {FancyLinkProps} from '@/app/_components/FancyLink';
 
 export interface I18nProps {
   blog: string;
@@ -20,34 +20,34 @@ export interface I18nProps {
   pronounce: string;
 }
 
-export default function HomeWrapper({ t, children }: { t: I18nProps; children: ReactNode }) {
+export default function HomeWrapper({t, children}: { t: I18nProps; children: ReactNode }) {
   const links: FancyLinkProps[] = [
-    { label: t.blog, href: '/blog', icon: <FaRegPenToSquare /> },
-    { label: t.telegram, href: 'https://t.me/aboutzy', target: '_blank', icon: <FaTelegramPlane /> },
-    { label: t.github, href: 'https://github.com/varzy', target: '_blank', icon: <FaGithub /> },
-    { label: t.instagram, href: 'https://instagram.com/varzyme', target: '_blank', icon: <FaInstagram /> },
-    { label: t.douban, href: 'https://www.douban.com/people/varzy/', target: '_blank', icon: <SiDoubanread /> },
+    {label: t.blog, href: '/blog', icon: <FaRegPenToSquare/>},
+    {label: t.telegram, href: 'https://t.me/aboutzy', target: '_blank', icon: <FaTelegramPlane/>},
+    {label: t.github, href: 'https://github.com/varzy', target: '_blank', icon: <FaGithub/>},
+    {label: t.instagram, href: 'https://instagram.com/varzyme', target: '_blank', icon: <FaInstagram/>},
+    {label: t.douban, href: 'https://www.douban.com/people/varzy/', target: '_blank', icon: <SiDoubanread/>},
     {
       label: t.neteaseMusic,
       href: 'https://music.163.com/playlist?id=39874340&userid=45403592',
-      icon: <RiNeteaseCloudMusicLine />,
+      icon: <RiNeteaseCloudMusicLine/>,
     },
-    { label: t.resume, href: '/resume', icon: <FaRegAddressCard /> },
+    {label: t.resume, href: '/resume', icon: <FaRegAddressCard/>},
   ];
 
-  const Row = ({ title, children }: { title?: string; children: ReactNode }) => (
-    <div className="mb-12 flex flex-wrap sm:mb-14">
-      <div className="left mb-3 flex w-full shrink-0 justify-start sm:mb-0 sm:w-1/6 sm:justify-end">
-        {title && <h1 className="text-base font-bold text-black">{title}</h1>}
+  const Row = ({title, children}: { title?: string; children: ReactNode }) => (
+    <div className="mb-12">
+      <div className="mb-3">
+        {title && <h1 className="text-base font-bold">{title}</h1>}
       </div>
-      <div className="right  flex-1 sm:mx-8">{children}</div>
+      <div className="">{children}</div>
     </div>
   );
 
   return (
     <>
-      <LocaleToggle />
-      <div className="container mx-auto max-w-4xl px-5 py-20 leading-7 sm:px-8">
+      <LocaleToggle/>
+      <div className="container mx-auto max-w-3xl px-5 py-20 leading-7 sm:px-16">
         <Row>
           <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between">
             <div className="info">
@@ -60,7 +60,7 @@ export default function HomeWrapper({ t, children }: { t: I18nProps; children: R
               <p>🧑‍💻Web Developer. 📝Blogger. 🫣INFJ.</p>
             </div>
             <div className="avatar flex justify-center md:justify-end">
-              <Avatar />
+              <Avatar/>
             </div>
           </div>
         </Row>
@@ -71,7 +71,7 @@ export default function HomeWrapper({ t, children }: { t: I18nProps; children: R
           <div className="flex flex-wrap">
             {links.map((link, index) => (
               <div className="mb-3 basis-1/2 sm:basis-1/3 md:basis-1/4" key={index}>
-                <FancyLink href={link.href} target={link.target} label={link.label} icon={link.icon} />
+                <FancyLink href={link.href} target={link.target} label={link.label} icon={link.icon}/>
               </div>
             ))}
           </div>

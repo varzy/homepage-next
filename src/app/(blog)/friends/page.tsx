a7885ff4b12143c57125a5e6c03e0866bbdcb2b4
zy <varzy@live.com>
2025-09-05 16:26:56 +0800

    Update Friends page content for improved clarity in the invitation to exchange links.

--- src/app/(blog)/friends/page.tsx
+++ src/app/(blog)/friends/page.tsx
@@ -15,7 +15,7 @@ export default function Friends() {
   return (
     <BlogPageContainer pageHero={{ title: `友情链接` }}>
       <Prose>
-        <p>欢迎互换友链。你可以发送邮件到 varzyme#gmail.com 告知我你的站点信息。以下是我的朋友们的站点，去看看吧。</p>
+        <p>欢迎互换友链，你可以发送邮件到 varzyme#gmail.com 告知我你的站点信息。以下是我的朋友们的站点，去看看吧。</p>
       </Prose>
 
       <div className="mt-8">
